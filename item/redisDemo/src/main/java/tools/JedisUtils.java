55220d3ddd393e9f3240112f2b026563d0452fb6
grain <laowu875054886@gmail.com>
2019-06-26 16:38:55 +0800

    redis jedis

---
+++ item/redisDemo/src/main/java/tools/JedisUtils.java
@@ -0,0 +1,29 @@
+package tools;
+
+import lombok.Data;
+import redis.clients.jedis.Jedis;
+
+import java.util.Objects;
+
+/**
+ * @author laowu
+ * @version demo
+ * 2019/6/26 16:09
+ */
+@SuppressWarnings("unused")
+@Data
+public class JedisUtils {
+    private String host = "localhost";
+    private int port = 6379;
+    private Jedis jedis;
+
+    private JedisUtils(){
+        if (Objects.isNull(jedis)) {
+            this.jedis = new Jedis(this.host, this.port);
+        }
+    }
+
+    public JedisUtils(Jedis jedis) {
+        this.jedis = jedis;
+    }
+}
