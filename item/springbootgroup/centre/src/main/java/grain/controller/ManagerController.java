7d2c7e4aa573276fa7fa98765482030a5e216369
Grain22 <laowu875054886@gmail.com>
2020-03-17 18:04:24 +0800

    分布式任务处理

--- item/springbootgroup/centre/src/main/java/grain/controller/ManagerController.java
+++ item/springbootgroup/centre/src/main/java/grain/controller/ManagerController.java
@@ -2,23 +2,31 @@ package grain.controller;
 
 import grain.constants.Strings;
 import grain.runs.ManageCenter;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import tools.net.AddressUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author laowu
  */
-@Controller
+@RestController
 @RequestMapping("center")
 public class ManagerController {
 
-    @PostMapping(Strings.REGISTER)
-    public void register(String info, HttpServletRequest request) {
+    @GetMapping(Strings.REGISTER)
+    public void register(String info, HttpServletRequest request, HttpServletResponse response) {
         String ipAddress = AddressUtils.getIPAddress(request);
         ManageCenter.addNode(ipAddress);
+        response.setStatus(HttpServletResponse.SC_OK);
+        try {
+            response.getWriter().write("register success");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
