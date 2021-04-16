85530b28b9c1fe765dcf395b2652b95ef39945c1
grain <laowu875054886@gmail.com>
2021-04-16 17:56:22 +0800

    移除配置记录

--- item/springbootconfig/src/main/java/grain/configs/resolver/ToResultStringInterface.java
+++
@@ -1,9 +0,0 @@
-package grain.configs.resolver;
-
-public interface ToResultStringInterface {
-    /**
-     * @param obj 转换前
-     * @return 字符串返回值
-     */
-    String toJsonString(Object obj);
-}
